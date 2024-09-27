768d2c44ba1998a0ddaeee6450e7ad9f2d9f6335
Juan David Valencia Moreno <98667658+Juanvalencia112@users.noreply.github.com>
2024-09-27 09:21:23 -0500

    Upload Header

---
+++ src/components/Header.js
@@ -0,0 +1,10 @@
+function Header() {
+  return (
+    <header>
+      <h1>Adopta un robot con Robot Lovers!</h1>
+      <img src={'https://img.freepik.com/vector-premium/conjunto-lindos-robots-vintage_331172-1430.jpg?w=996'} alt="Robots" />
+    </header>
+  )
+}
+
+export default Header;
